759243d6b9b297fd542626f5c0bc4d5269f06288
zxfeng <fl_zxf@163.com>
2019-04-30 16:10:26 +0800

    login-form up

--- antd-test/parcel-login-form-test/src/index.js
+++ antd-test/parcel-login-form-test/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import './index.css';
 import {
-  Form, Icon, Input, Button, Checkbox,
+  Form, Icon, Input, Button, Checkbox
 } from 'antd';
 
 class NormalLoginForm extends React.Component {
@@ -22,16 +22,16 @@ class NormalLoginForm extends React.Component {
       <Form onSubmit={this.handleSubmit} className="login-form">
         <Form.Item>
           {getFieldDecorator('userName', {
-            rules: [{ required: true, message: 'Please input your username!' }],
+            rules: [{ required: true, message: '请输入账号！' }],
           })(
-            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+            <Input prefix={<Icon type="user" />} placeholder="账号" />
           )}
         </Form.Item>
         <Form.Item>
           {getFieldDecorator('password', {
-            rules: [{ required: true, message: 'Please input your Password!' }],
+            rules: [{ required: true, message: '请输入密码！' }],
           })(
-            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+            <Input.Password prefix={<Icon type="lock" />} allowClear placeholder="密码" />
           )}
         </Form.Item>
         <Form.Item>
@@ -39,13 +39,11 @@ class NormalLoginForm extends React.Component {
             valuePropName: 'checked',
             initialValue: true,
           })(
-            <Checkbox>Remember me</Checkbox>
+            <Checkbox>记住我</Checkbox>
           )}
-          <a className="login-form-forgot" href="">Forgot password</a>
-          <Button type="primary" htmlType="submit" className="login-form-button">
-            Log in
-          </Button>
-          Or <a href="">register now!</a>
+          <a className="login-form-forgot" href="#">忘记密码</a>
+          <Button type="primary" htmlType="submit" className="login-form-button">登录</Button>
+          或 <a href="#">现在注册！</a>
         </Form.Item>
       </Form>
     );
