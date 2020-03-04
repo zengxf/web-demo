c009f4bb61fb3296331ff4b6bbe28aa152fe270a
feng <fl_zxf@sina.cn>
2020-03-04 12:21:37 +0800

    taro

--- wx-mini-app-test/taro-test/readme.md
+++ wx-mini-app-test/taro-test/readme.md
@@ -6,5 +6,14 @@
 - 微信运行：`npm run dev:weapp`
   - **可以用微信测试**
 
+## 初始化
+- `taro init taro-ui-test` 先设置名称
+- `taro init` 交互式设置名称
+- **注**：其会初始化 `git`
+  - 所以，根据需要删除 `.git` 文件夹
+
 ## page 配置
 - 在 `src/app.jsx` 文件中的 `config.pages` 配置
+
+## 使用 Taro-UI
+- `npm install taro-ui`
--- wx-mini-app-test/taro-test/taro-h5/src/pages/index/index.jsx
+++ wx-mini-app-test/taro-test/taro-h5/src/pages/index/index.jsx
@@ -16,11 +16,27 @@ export default class Index extends Component {
         })
     }
 
+    login = () => {
+        console.info("login")
+        Taro.login({
+            success: function (res) {
+                if (res.code) {
+                    // 发起网络请求
+                    console.info("code =>", res)
+                    console.info("env =>", Taro.getEnv())
+                } else {
+                    console.log('登录失败！' + res.errMsg)
+                }
+            }
+        })
+    }
+
     render() {
         return (
             <View className='index'>
                 <Text>Hello world!</Text>
                 <AtButton onClick={this.test} type='primary'>按钮文案</AtButton>
+                <AtButton onClick={this.login} type='primary'>登录</AtButton>
             </View>
         )
     }
