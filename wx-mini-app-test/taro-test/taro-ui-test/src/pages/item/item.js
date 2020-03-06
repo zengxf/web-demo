46045ede628c5acdccf9aea3bcb5e4239e86f626
feng <fl_zxf@sina.cn>
2020-03-06 18:17:22 +0800

    markdown

--- wx-mini-app-test/taro-test/taro-ui-test/src/pages/item/item.js
+++ wx-mini-app-test/taro-test/taro-ui-test/src/pages/item/item.js
@@ -1,6 +1,7 @@
-import Taro, { Component } from '@tarojs/taro'
 import { View } from '@tarojs/components'
-import { AtList, AtListItem, AtToast, AtDivider } from "taro-ui"
+import { Component } from '@tarojs/taro'
+import 'taro-parse/dist/style/main.scss'
+import TaroParser from 'taro-parse'
 
 const http = require('../../utils/http.js')
 const format = require('../../utils/format.js')
@@ -26,19 +27,53 @@ export default class Item extends Component {
     }
 
     fetch = () => {
-        var rt = '# h1\n## h2\n- test\n  - test-11'
+        // var rt = '# h1\n## h2\n- test\n  - test-11'
+        var rt = `
+# Markdown 
+- ddd
+  - ssb
+  - 334
+1. abc
+2. bcd
+    - efg
+- [百度](https://www.baidu.com)
+- 公式1 $a = b - \\sqrt{c}$
+- 公式2 
+    - $$ a = b - \\sqrt{c} $$
+- aa: \`bb\`
+
+## code
+\`\`\`
+function test() {
+    return "test";
+}
+\`\`\`
+    `
         console.info("rt", rt)
         this.setState({ richText: rt })
-        // this.setState({ loading: true })
-        // http.get("/api/note/review/find-list", { status: this.state.listSign, date: this.state.dateStr }, res => {
-        //     this.setState({ loading: false, data: res })
-        // })
+    }
+    imgClick = (src) => {
+        Taro.previewImage({ urls: [src] })
+    }
+    linkClick = (href) => {
+        Taro.setClipboardData({ data: href }).then(() => {
+            Taro.showToast({ title: '链接已复制' })
+        })
     }
 
     render() {
         return (
             <View >
-                <wemark md={this.state.richText} link highlight type='wemark' />
+                {/* <wemark md={this.state.richText} link highlight type='wemark' /> */}
+                <TaroParser
+                    type='markdown'
+                    theme='light'
+                    onImgClick={this.imgClick}
+                    onLinkClick={this.linkClick}
+                    yumlApi='https://md.werfei.com/?yuml'
+                    latexApi='https://md.werfei.com/?tex'
+                    content={this.state.richText}
+                />
             </View>
         )
     }
